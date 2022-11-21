29d40a77b395fd1402ed443441f0443f77381da6
skakac <uncut.wz@gmail.com>
2022-11-21 15:04:34 +0100

    Custom trusted token list provider

---
+++ src/providers/ProviderTrusted.ts
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import _ from 'lodash'
+
+import { Tag, TokenSet } from '../types'
+
+import { Provider } from './Provider'
+
+interface TrustedListToken {
+    chainId: number
+    name: string
+    symbol: string
+    logoURI: string
+    tags: Tag[]
+    address: string
+}
+
+interface TrustedList {
+    tokens: TrustedListToken[]
+}
+
+export class ProviderTrusted extends Provider {
+    constructor(
+        private readonly url: string,
+        private readonly skipTags: Tag[], // Filter out specific tags
+        private readonly chainId: number = 101 // Filter by chain id
+    ) {
+        super()
+    }
+
+    async getTokens(): Promise<TokenSet> {
+        const tokenMap = new TokenSet()
+
+        const tokens = await axios.get<TrustedList>(this.url)
+        for (let i = 0; i < tokens.data.tokens.length; i++) {
+            const token: TrustedListToken = tokens.data.tokens[i]
+
+            // Get only tokens for mainnet and devnet
+            if (
+                this.chainId === token.chainId &&
+                !_.intersection(token.tags, this.skipTags).length
+            ) {
+                tokenMap.set({
+                    chainId: token.chainId,
+                    name: token.name,
+                    symbol: token.symbol,
+                    address: token.address,
+                    decimals: null,
+                    logoURI: token.logoURI,
+                    tags: new Set<Tag>(token.tags),
+                    verified: true,
+                    holders: null,
+                })
+            }
+        }
+
+        return tokenMap
+    }
+}
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,3 +1,4 @@
 export * from './Provider'
 export * from './ProviderCoinGecko'
 export * from './ProviderLegacyToken'
+export * from './ProviderTrusted'
